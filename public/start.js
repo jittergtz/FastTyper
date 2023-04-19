111fdc279ecc9712eee519b33d361bd33bf7dcdc
sandro <110451040+jittergtz@users.noreply.github.com>
2023-04-20 00:18:39 +0200

    Update start.js

--- public/start.js
+++ public/start.js
@@ -1,3 +1,8 @@
+const gameTime = 60 * 1000;
+window.timer = null;
+window.gameStart = null
+
+
 
 
 fetch('json/german1k.json')
@@ -6,8 +11,8 @@ fetch('json/german1k.json')
     const words = data.words;
     const wordParentElement = document.getElementById('words');
     
-// loop to generate 150 random words and append to wordParentElement
-for (let i = 0; i < 150; i++) {
+// loop to generate 200 random words and append to wordParentElement
+for (let i = 0; i < 200; i++) {
   const randomWord = words[Math.floor(Math.random() * words.length)];
   const randomWordDiv = document.createElement('div');
   randomWordDiv.classList.add('word'); // add the "word" class to the div
@@ -53,13 +58,14 @@ for (let i = 0; i < 150; i++) {
   function removeClass(el,name) {
     el.className = el.className.replace(name,'');
   }
-  
+  2
+ 
   
   
   
   // game beginning
-  
-  document.getElementById('game').addEventListener('keyup', ev => {
+    
+    document.getElementById('game').addEventListener('keyup', ev => {
     const key = ev.key;
     const currentWord = document.querySelector('.word.current');
     const currentLetter = document.querySelector('.letter.current');
@@ -68,6 +74,30 @@ for (let i = 0; i < 150; i++) {
     const isSpace = key === ' ';
     const isBackspace = key === "Backspace";
     const isFirstLetter = currentLetter === currentWord.firstChild;
+
+   // timer 
+    if (!window.timer && isLetter || " ") {
+      window.timer = setInterval(() => {
+        if (!window.gameStart) {
+          window.gameStart = (new Date()).getTime();
+        }
+        const currentTime = (new Date()).getTime();
+        const msPassed = currentTime - window.gameStart;
+        const sPassed = Math.round(msPassed / 1000);
+        const sLeft = Math.round((gameTime / 1000) - sPassed);
+        if (sLeft <= 10){
+         clock = document.querySelector(".clock")
+         clock.style.color = "rgba(203, 70, 53, 0.804)"
+        }
+        if (sLeft <= -1) {
+          gameOver();
+          return;
+        }
+        document.getElementById('timer').innerHTML = sLeft + 's';
+      }, 1000);
+    }
+  
+
   
     if (isBackspace){
       if(currentLetter && isFirstLetter){
@@ -98,12 +128,7 @@ for (let i = 0; i < 150; i++) {
         if (currentLetter.nextSibling) {
           addClass(currentLetter.nextSibling, 'current');
         }
-      } else {
-        const incorrectLetter = document.createElement('span');
-        incorrectLetter.innerHTML = key;
-        incorrectLetter.className = 'letter incorrect extra';
-        currentWord.appendChild(incorrectLetter);
-      }
+      } 
     }
     
   //space beginning
@@ -126,35 +151,54 @@ for (let i = 0; i < 150; i++) {
 // space end 
 
 
+      //cursor
+      const nextLetter = document.querySelector('.letter.current');
+      const nextWord = document.querySelector('.word.current');
+      const cursor = document.getElementById('cursor');
+      cursor.style.top = (nextLetter || nextWord).getBoundingClientRect().top + 2 + 'px';
+      if(!nextLetter){
+       cursor.style.left = (nextLetter || nextWord).getBoundingClientRect()[nextLetter ? 'left' : 'right'] - 8 + 'px';
+
+      }else{
+      cursor.style.left = (nextLetter || nextWord).getBoundingClientRect()[nextLetter ? 'left' : 'right'] + 'px';
+      }
+     
 
 
-      
-
-       //cursor
-       const nextLetter = document.querySelector('.letter.current');
-       const nextWord = document.querySelector('.word.current');
-       const cursor = document.getElementById('cursor');
-       cursor.style.top = (nextLetter || nextWord).getBoundingClientRect().top + 2 + 'px';
-       cursor.style.left = (nextLetter || nextWord).getBoundingClientRect()[nextLetter ? 'left' : 'right'] + 'px';
 
 
-
-
-
+                // 190 var parameter zum einstellen des line wechsel 
           // move lines / words
-  if (currentWord.getBoundingClientRect().top > 220) {
+  if (currentWord.getBoundingClientRect().top > 190) { 
     const words = document.querySelector('.word_parent');
     const margin = parseInt(words.style.marginTop || '0px');
      words.style.marginTop = (margin - 33) + 'px';
   }
   
    // glaub ich muss words auf eine klasse reduzieren und word parents entfernen wegen css undso
-   
+
 });
   // game end
- 
-      
 
+
+
+
+
+
+
+
+
+  
+
+ 
+  const game = document.getElementById("game");
+
+  game.addEventListener("keydown", function(event) {
+    if (event.key === " ") {
+      event.preventDefault();
+    }
+  });
+                                              
 
 
 
@@ -167,6 +211,7 @@ for (let i = 0; i < 150; i++) {
         document.body.style.position = '';
         document.body.style.top = '';
         window.scrollTo(0, parseInt(scrollY || '0') * -1);
+
 
 
 
